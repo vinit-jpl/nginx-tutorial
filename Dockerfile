413d37d1207bdbdde76ab861d797ac8a9c38b6a7
vinit-jpl <vinit.jpl@gmail.com>
2025-12-01 23:19:42 +0530

    add Dockerfile and docker-compose.yml; update server.js for environment variable usage

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:20
+
+WORKDIR /app
+
+COPY server.js .
+COPY index.html  .
+COPY images ./images
+COPY package.json .
+
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD ["node", "server.js"]
+
--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const path = require("path")
 const app = express()
 const port = 3000
 
+const appReplica = process.env.APP_NAME
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
-    console.log("Request serverd by node app")
+    console.log(`Request serverd by ${appReplica}`)
 })
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`)
+    console.log(`${appReplica} started on port ${port}`)
 })
